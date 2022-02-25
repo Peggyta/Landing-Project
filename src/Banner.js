ed054f5e3be562a16bfdac124bb1725425076e69
Peggyta <peggyta9096@gmail.com>
2022-02-25 22:20:09 +0330

    banner & information section added to the project

--- src/Banner.js
+++ src/Banner.js
@@ -5,10 +5,16 @@ import banner from "./images/background.jpg";
 const Banner = () => {
     return (
         <div className={styles.container}>
-            <img src={banner} alt="banner"/>
+            <div className={styles.imgContainer}>
+                <img src={banner} alt="banner"/>
+            </div>
+    
             <div className={styles.textContainer}>
+                <div className={styles.infoContainer}>
                 <h3>Lorem ipsum dolor sit amet</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+                </div>
+                
             </div>
         </div>
     );
