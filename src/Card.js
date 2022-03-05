a98750324777660abb0c676db4d767e48e769e09
Peggyta <peggyta9096@gmail.com>
2022-03-05 22:16:48 +0330

    counter section added to the project

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import styles from "./Card.module.css";
+import plus from "./images/plus.svg";
+import minus from "./images/minus.svg";
 
 class Card extends Component {
     render() {
         const {image, name, price} = this.props;
+        const {counter} = this.state;
         return (
             <div className={styles.Container}>
                 <img src={image} alt="products"/>
                 <h3>{name}</h3>
                 <p>{price}</p>
+                <div className={styles.Counter}>
+                    <img src={minus} alt="remove item"/>
+                    <span>{counter}</span>
+                    <img src={plus} alt="add item"/>
+                </div>
             </div>
         );
     }
