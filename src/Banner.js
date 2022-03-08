5f9b69276392ec6935b44c668ed4be95f57137c4
Peggyta <peggyta9096@gmail.com>
2022-03-08 22:57:10 +0330

    responsive css for tablets added to banner section

--- src/Banner.js
+++ src/Banner.js
@@ -9,6 +9,7 @@ class Banner extends Component{
             <div className={styles.container}>
            <div className={styles.contentContainer}>
                 <img src={banner} alt="banner"/>
+                <p className={styles.infoContainer}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.</p>
                 <div className={styles.textContainer}><h3>Lorem ipsum dolor sit amet</h3>
                     <br/>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.</p>
